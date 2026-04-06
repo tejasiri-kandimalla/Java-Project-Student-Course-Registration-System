e207e1797df06aaf480d4d23d5eb489cb4a75091
Teja Siri Kandimalla <tejasiri-kandimalla@github.com>
2026-04-06 16:01:19 +0530

    Update database connection credentials

--- src/studentcourseregistrationsystem/DatabaseConnection.java
+++ src/studentcourseregistrationsystem/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/student_course_db";
-    private static final String USER = "root";
+    private static final String USER = "admin";
     private static final String PASS = "12345"; // Updated password
 
     static {
